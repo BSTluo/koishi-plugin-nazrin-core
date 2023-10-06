c7a1c9ada1cd25206a16397cb81786d2ed2ed36c
BSTluo <1946831552@qq.com>
2023-10-06 21:38:32 +0800

    modified:   src/pageHtmlMake.ts

--- src/pageHtmlMake.ts
+++ src/pageHtmlMake.ts
@@ -1,50 +1,56 @@
-export const pageMake = (obj) => {
-  let strTemp = ''
-  obj.forEach((element, index) => {
-    strTemp = strTemp + `
-        <div class="item">
-        <div class="seq">${index + 1}.</div>
-        <div class="name">${element.name}</div>
-        <span>---</span>
-        <div class="author">${element.author}</div>
-        <span>来自:</span>
-        <div class="app">${element.platform}</div>
-        </div>\n`
-  });
+export const pageMake = (list) => {
+  const header = `
+      <div class="box">
+          <div class="title">Nazrin-search</div>
+          <div class="grid">
+              <div class="category">编号</div>
+              <div class="category">名称</div>
+              <div class="category">作者</div>
+              <div class="category">平台</div>
+  `;
 
-  const data = `
-  <div class="box">
-  <div class="title">Nazrin-search</div>
-  ${strTemp}\n
-  </div>
-  <style>
-  .box {
-    border: #37474f 2px solid;
-    color: #37474f;
-    background: #f3f3f3;
-    display: inline-block;
-    padding: 8px 0 12px 0;
-  }
+  const items = list.map((item, index) => `
+      <div class="content">${index + 1}</div>
+      <div class="content">${item.name}</div>
+      <div class="content">${item.author}</div>
+      <div class="content">${item.platform}</div>
+  `).join('');
 
-  .title {
-    color: #CE9178;
-    width: 100%;
-    text-align: center;
-  }
+  const footer = `
+          </div>
+      </div>
+      <style>
+          .box {
+              border: #37474f 2px solid;
+              color: #37474f;
+              background: #f3f3f3;
+              display: inline-block;
+              padding: 8px 12px 12px 12px;
+          }
+          .title {
+              color: #CE9178;
+              width: 100%;
+              font-size: 16px;
+              font-weight: bold;
+              text-align: center;
+          }
+          .grid {
+              display: grid;
+              grid-template-rows: auto 1fr 1fr 1fr;
+              grid-template-columns: repeat(4, 1fr);
+              gap: 8px;
+              margin: 12px 0;
+              white-space: nowrap;
+          }
+          .category {
+              font-weight: bold;
+              text-align: center;
+          }
+          .content {
+              text-align: center;
+          }
+      </style>
+  `;
 
-  .item {
-    width: 100%;
-    margin: 0 12px;
-  }
-
-  .item>div,
-  .item>span {
-    display: inline-block;
-  }
-
-  .seq {}
-
-  .app {}
-  </style>`
-  return data
+  return header + items + footer;
 }
